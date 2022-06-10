d169e4d1cedaf011225aa5cf1b998e60c1d527f5
John <jrborges52@email.com>
2022-06-10 00:59:09 +0000

    finally

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,23 @@
-const input = process.argv.slice(2);
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=` + breedName, (error, response, body) => {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=` + input, (error, response, body) => {
   
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  
-  console.log(typeof body);
-  const data = JSON.parse(body);
-  if (data.length > 0) {
-    console.log(data[0].description);
-  } else {
-    console.log("ERROR, YOU PUT THE WRONG BREED");
+    
+    
+    if (error) {
+      return callback(error, null)
+    }
+    const data = JSON.parse(body); //[{}]
+    
+    if (!data.length) {
+        return callback("invalid", null)
+    } 
 
-  }
+  return callback (null, data[0].description)
+   
+  })  
   
-});
+};
+module.exports = {fetchBreedDescription };
